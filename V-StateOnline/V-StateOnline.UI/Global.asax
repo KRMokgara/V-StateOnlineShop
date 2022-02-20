ffe91ff64d276db5de638e39c502e4db0657feeb
KRMokgara <pablobotek808@gmail.com>
2022-02-20 14:19:05 +0200

    Initial Solution Setup

---
+++ V-StateOnline/V-StateOnline.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="V_StateOnline.UI.MvcApplication" Language="C#" %>
---
+++ V-StateOnline/V-StateOnline.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(V_StateOnline.UI.Startup))]
+namespace V_StateOnline.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
